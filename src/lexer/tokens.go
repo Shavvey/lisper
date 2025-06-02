86b38258f1eefe114630359de760335871e14a61
Cole Johnson <coledjohn@gmail.com>
2025-06-02 16:09:30 -0600

    adding `AtomType`

--- src/lexer/tokens.go
+++ src/lexer/tokens.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "fmt"
+
 // main lexer type interface
 type Token interface {
 	toString() string
@@ -20,6 +22,21 @@ const (
 	Keyword
 	Symbol
 )
+
+func AtomTypeToStr(t AtomType) string {
+	switch t {
+	case LeftParen:
+		return "LEFT_PAREN"
+	case RightParen:
+		return "RIGHT_PAREN"
+	case Keyword:
+		return "KEYWORD"
+	case Symbol:
+		return "SYMBOL"
+	default:
+		panic(fmt.Errorf("Unknown type: %d", t))
+	}
+}
 
 // atom is just a singular token (everything should resolve to this)
 type Atom struct {
