beefe8982fcfe37c59791284139246b2fe41e73e
Cole Johnson <coledjohn@gmail.com>
2025-06-02 16:01:30 -0600

    adding `Token` interface that lexer will use

--- src/lexer/tokens.go
+++ src/lexer/tokens.go
@@ -1,10 +1,28 @@
 package lexer
 
-type Token string
+// main lexer type interface
+type Token interface {
+	toString() string
+}
 
-// defining static tokens
+// list is just a collection of other tokens
+type List struct {
+	items []Token
+}
 
+// types of atoms we can have in our lang
+type AtomType int
+
+// NOTE: expand this later
 const (
-	LeftParen  = "("
-	RightParen = ")"
+	LeftParen AtomType = iota
+	RightParen
+	Keyword
+	Symbol
 )
+
+// atom is just a singular token (everything should resolve to this)
+type Atom struct {
+	content string
+	AtomType
+}
